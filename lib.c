c10c9e07f1c8260d8bf514788b6c10ef8d13fce9
Olaf Flebbe <of@oflebbe.de>
2024-12-24 21:53:58 +0100

    refactor

---
+++ lib.c
@@ -0,0 +1,19 @@
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_INCLUDE_STANDARD_BOOL
+#define NK_IMPLEMENTATION
+#define NK_SDL_GL3_IMPLEMENTATION
+#include <GL/glew.h>
+#include "nuklear.h"
+#include "nuklear_sdl_gl3.h"
+
+#define FLO_QUEUE_IMPLEMENTATION
+#include "flo_queue.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
